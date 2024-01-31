0b3d0ba601c04a24c4bffdb4dc5e79219e313f80
rqure <64617806+rqure@users.noreply.github.com>
2024-01-31 09:09:11 -0700

    Update main.go

--- main.go
+++ main.go
@@ -12,42 +12,18 @@ import (
 )
 
 func main() {
-	app := qmq.NewQMQApplication("logger")
+	app := qmq.NewQMQApplication("audio-remote")
 	app.Initialize()
 	defer app.Deinitialize()
 
-	key := os.Getenv("APP_NAME") + ":logs"
-	app.AddConsumer(key).Initialize()
-	app.Consumer(key).ResetLastId()
-
-	tickRateMs, err := strconv.Atoi(os.Getenv("TICK_RATE_MS"))
-	if err != nil {
-		tickRateMs = 100
-	}
+	app.AddProducer("audio-remote:exchange").Initialize(10)
 
 	sigint := make(chan os.Signal, 1)
 	signal.Notify(sigint, os.Interrupt)
 
 	log.SetFlags(log.Lmicroseconds)
 
-	ticker := time.NewTicker(time.Duration(tickRateMs) * time.Millisecond)
-	for {
-		select {
-		case <-sigint:
-			app.Logger().Advise("SIGINT received")
-			return
-		case <-ticker.C:
-			logMsg := &qmq.QMQLog{}
-
-			for {
-				popped := app.Consumer(key).Pop(logMsg)
-				if popped == nil {
-					break
-				}
-
-				log.Printf("%s | %s | %s", logMsg.Application, strings.Replace(logMsg.Level.String(), "LOG_LEVEL_", "", -1), logMsg.Message)
-				popped.Ack()
-			}
-		}
-	}
+	app.Producer("audio-remote:exchange").Push(&qmq.QMQAudioRequest{
+		Filename: os.Getenv("AUDIO_FILE"),
+	})
 }
