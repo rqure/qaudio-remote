e38251266d8447d3a49e5bd3dc94fd71ce002749
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-04-09 23:36:24 -0600

    some fixes

--- main.go
+++ main.go
@@ -1,36 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-
 	qmq "github.com/rqure/qmq/src"
 )
 
+type NameProvider struct{}
+
+func (np *NameProvider) Get() string {
+	return "audio-remote"
+}
+
 func main() {
-	app := qmq.NewQMQApplication("audio-remote")
-	app.Initialize()
-	defer app.Deinitialize()
-
-	app.AddProducer("audio-player:file:exchange").Initialize(10)
-	app.AddProducer("audio-player:tts:exchange").Initialize(10)
-
-	log.SetFlags(log.Lmicroseconds)
-
-	audioFile := os.Getenv("AUDIO_FILE")
-	if audioFile != "" {
-		app.Logger().Advise(fmt.Sprintf("Sending request to play: %s", audioFile))
-		app.Producer("audio-player:file:exchange").Push(&qmq.QMQAudioRequest{
-			Filename: audioFile,
-		})
-	}
-
-	text := os.Getenv("TEXT_TO_SPEECH")
-	if text != "" {
-		app.Logger().Advise(fmt.Sprintf("Sending request to text to speech: %s", text))
-		app.Producer("audio-player:tts:exchange").Push(&qmq.QMQTextToSpeechRequest{
-			Text: text,
-		})
-	}
+	engine := qmq.NewDefaultEngine(qmq.DefaultEngineConfig{
+		NameProvider:               &NameProvider{},
+		TransformerProviderFactory: &TransformerProviderFactory{},
+		EngineProcessor:            &EngineProcessor{},
+	})
+	engine.Run()
 }
