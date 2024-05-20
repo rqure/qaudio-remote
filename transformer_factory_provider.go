fbabdf578338e7f03eb11397781a699e2dda1e57
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-05-20 17:35:58 -0600

    some progress

--- transformer_factory_provider.go
+++ transformer_factory_provider.go
@@ -8,11 +8,15 @@ func (t *TransformerProviderFactory) Create(components qmq.EngineComponentProvid
 	transformerProvider := qmq.NewDefaultTransformerProvider()
 	transformerProvider.Set("producer:audio-player:cmd:play-file", []qmq.Transformer{
 		NewAudioRequestToAnyTransformer(components.WithLogger()),
-		qmq.NewAnyToMessageTransformer(components.WithLogger()),
+		qmq.NewAnyToMessageTransformer(components.WithLogger(), qmq.AnyToMessageTransformerConfig{
+			SourceProvider: components.WithNameProvider(),
+		}),
 	})
 	transformerProvider.Set("producer:audio-player:cmd:play-tts", []qmq.Transformer{
 		NewTtsRequestToAnyTransformer(components.WithLogger()),
-		qmq.NewAnyToMessageTransformer(components.WithLogger()),
+		qmq.NewAnyToMessageTransformer(components.WithLogger(), qmq.AnyToMessageTransformerConfig{
+			SourceProvider: components.WithNameProvider(),
+		}),
 	})
 	return transformerProvider
 }
